ec7935cd3859f030fa56dafb411e159eca6e5122
yegorgursky <yahorhurski@gmail.com>
2022-07-09 19:38:17 +0300

    edit filter buttons by using material ui

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {Button} from "./components/Button";
+// import {Button} from "./components/Button";
 import {Checkbox} from "./components/Checkbox";
 import styles from "./Todolist.module.css";
 import {AddItemForm} from "./components/AddItemForm";
 import {EditableSpan} from "./components/EditableSpan";
+import Stack from '@mui/material/Stack/Stack';
+import Button from '@mui/material/Button';
 
 export type TaskType = {
     id: string;
@@ -73,13 +75,24 @@ export const Todolist = (props: PropsType) => {
         props.removeTodolist(props.todolistID);
     }
 
+    const styleButtonAll =
+        props.taskFilter === Filter.All
+            ? {background: '#1977d2', color: 'white'}
+            : {background: 'none'}
+    const styleButtonActive = props.taskFilter === Filter.Active
+        ? {background: '#1977d2', color: 'white'}
+        : {background: 'none'}
+    const styleButtonCompleted = props.taskFilter === Filter.Completed
+        ? {background: '#1977d2', color: 'white'}
+        : {background: 'none'}
+
     return (
         <div>
             <h3>
                 <EditableSpan value={props.todolistTitle}
                               callback={(value) => editTodolistTitle(value)}
                 />
-                <Button buttonTitle={'x'} callback={() => removeTodolist()}/>
+                {/*<Button buttonTitle={'x'} callback={() => removeTodolist()}/>*/}
             </h3>
             <span className={styles.errorMessage}>{error}</span>
             <AddItemForm addFormCallback={addTask}/>
@@ -91,7 +104,7 @@ export const Todolist = (props: PropsType) => {
                                       className={styles.btn}
                                       callback={(isDone) => onCheckboxHandler(task.id, isDone)}
                             />
-                            <Button buttonTitle={'x'} callback={() => removeTask(task.id)}/>
+                            {/*<Button buttonTitle={'x'} callback={() => removeTask(task.id)}/>*/}
                             <EditableSpan value={task.taskTitle}
                                           callback={(value) => editTaskTitle(task.id, value)}
                             />
@@ -100,19 +113,30 @@ export const Todolist = (props: PropsType) => {
                 })}
             </ul>
             <div className={styles.btn}>
-                <Button buttonTitle={Filter.All}
-                        callback={() => onChangeFilter(props.todolistID, Filter.All)}
-                        className={props.taskFilter === Filter.All ? styles.activeFilter : ''}/>
-                <Button buttonTitle={Filter.Active}
-                        callback={() => {
-                            onChangeFilter(props.todolistID, Filter.Active)
-                        }}
-                        className={props.taskFilter === Filter.Active ? styles.activeFilter : ''}/>
-                <Button buttonTitle={Filter.Completed}
-                        callback={() => {
-                            onChangeFilter(props.todolistID, Filter.Completed)
-                        }}
-                        className={props.taskFilter === Filter.Completed ? styles.activeFilter : ''}/>
+                <Stack spacing={1} direction="row">
+                    <Button variant="outlined"
+                            style={styleButtonAll}
+                            onClick={() => onChangeFilter(props.todolistID, Filter.All)}>{Filter.All}</Button>
+                    <Button variant="outlined"
+                            style={styleButtonActive}
+                            onClick={() => onChangeFilter(props.todolistID, Filter.Active)}>{Filter.Active}</Button>
+                    <Button variant="outlined"
+                            style={styleButtonCompleted}
+                            onClick={() => onChangeFilter(props.todolistID, Filter.Completed)}>{Filter.Completed}</Button>
+                </Stack>
+                {/*<Button buttonTitle={Filter.All}*/}
+                {/*        callback={() => onChangeFilter(props.todolistID, Filter.All)}*/}
+                {/*        className={props.taskFilter === Filter.All ? styles.activeFilter : ''}/>*/}
+                {/*<Button buttonTitle={Filter.Active}*/}
+                {/*        callback={() => {*/}
+                {/*            onChangeFilter(props.todolistID, Filter.Active)*/}
+                {/*        }}*/}
+                {/*        className={props.taskFilter === Filter.Active ? styles.activeFilter : ''}/>*/}
+                {/*<Button buttonTitle={Filter.Completed}*/}
+                {/*        callback={() => {*/}
+                {/*            onChangeFilter(props.todolistID, Filter.Completed)*/}
+                {/*        }}*/}
+                {/*        className={props.taskFilter === Filter.Completed ? styles.activeFilter : ''}/>*/}
             </div>
         </div>
     );
