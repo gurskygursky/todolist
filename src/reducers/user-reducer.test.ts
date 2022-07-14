9bdc4eaec8263c603c444c01fb6889bde67340e8
yegorgursky <yahorhurski@gmail.com>
2022-07-14 22:16:05 +0300

    create actions and action types, fixed tests

--- src/reducers/user-reducer.test.ts
+++ src/reducers/user-reducer.test.ts
@@ -1,26 +1,29 @@
-import {userReducer} from './user-reducer';
+import {changeNameAC, incrementAgeAC, incrementChildrenCountAC, userReducer} from './user-reducer';
 
 test('user reducer should increment only age', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
 
-    const endState = userReducer(startState, { type: 'INCREMENT-AGE' })
+    const endState = userReducer(startState, incrementAgeAC(startState.age + 1));
 
     expect(endState.age).toBe(21);
     expect(endState.childrenCount).toBe(2);
+    expect(endState.name).toEqual('Dimych');
 });
 
 test('user reducer should increment only childrenCount', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
     // your code here
-    const endState = userReducer(startState, { type: 'INCREMENT-CHILDREN-COUNT' })
+    const endState = userReducer(startState, incrementChildrenCountAC(startState.childrenCount + 1));
 
     expect(endState.age).toBe(20);
     expect(endState.childrenCount).toBe(3);
+    expect(endState.name).toEqual('Dimych');
 });
 test('user reducer should change name of user', () => {
     const startState = {age: 20, childrenCount: 2, name: 'Dimych' };
     const newName = 'Viktor';
-    const endState = userReducer(startState, { type: 'CHANGE-NAME', newName: newName })
+    const endState = userReducer(startState, changeNameAC(newName));
 
     expect(endState.name).toBe(newName);
+    expect(endState.name).toEqual('Viktor');
 });
--- src/reducers/user-reducer.ts
+++ src/reducers/user-reducer.ts
@@ -3,23 +3,58 @@ type StateType = {
     childrenCount: number
     name: string
 }
-type ActionType = {
-    type: string
-    [key: string]: any
-}
+// type ActionType = {
+//     type: string
+//     [key: string]: any
+// }
 
-export const userReducer = (state: StateType, action: ActionType) => {
+export const userReducer = (state: StateType, action: UserReducerActionType) => {
     switch (action.type) {
-        case 'INCREMENT-AGE':
-            state.age = state.age + 1;
-            return state;
-        case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1;
-            return state;
-        case 'CHANGE-NAME':
-            state.name = 'Viktor';
-            return state
+        case 'INCREMENT_AGE': return {
+            ...state, age: state.age + 1
+        }
+            // state.age = state.age + 1;
+            // return state;
+        case 'INCREMENT_CHILDREN_COUNT': return {
+                ...state, childrenCount: state.childrenCount + 1
+            }
+            // state.childrenCount = state.childrenCount + 1;
+            // return state;
+        case 'CHANGE-NAME': return  {
+            ...state, name: 'Viktor'
+        }
+            // state.name = 'Viktor';
+            // return state
         default:
             throw new Error("I don't understand this type")
     }
 }
+
+//actions
+export const incrementAgeAC = (age: number) => {
+    return {
+        type: 'INCREMENT_AGE',
+        payload: {age},
+    } as const
+}
+export const incrementChildrenCountAC = (childrenCount: number) => {
+    return {
+        type: 'INCREMENT_CHILDREN_COUNT',
+        payload: {childrenCount},
+    } as const
+}
+export const changeNameAC = (name: string) => {
+    return {
+        type: 'CHANGE-NAME',
+        payload: {name},
+    } as const
+}
+//action types
+export type IncrementAgeActionType = ReturnType<typeof incrementAgeAC>;
+export type IncrementChildrenCountActionType = ReturnType<typeof incrementChildrenCountAC>;
+export type ChangeNameActionType = ReturnType<typeof changeNameAC>;
+
+export type UserReducerActionType =
+    IncrementAgeActionType
+    | IncrementChildrenCountActionType
+    | ChangeNameActionType;
