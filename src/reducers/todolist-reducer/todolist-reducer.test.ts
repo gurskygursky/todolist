8365f64aebc2b6752f167c87828be31d77a841ac
yegorgursky <yahorhurski@gmail.com>
2022-08-19 14:28:34 +0300

    todolist reducer tests

---
+++ src/reducers/todolist-reducer/todolist-reducer.test.ts
@@ -0,0 +1,87 @@
+import {
+    addTodolistAC, changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC,
+    todolistReducer
+} from '../todolist-reducer/todolist-reducer';
+import {v1} from 'uuid';
+import {TaskFilterType, TodolistType} from '../../Todolist';
+
+test('correct todolist should be removed', () => {
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    const startState: TodolistType[] = [
+        {id: todolistID1, todolistTitle: "What to learn", filter: "All"},
+        {id: todolistID2, todolistTitle: "What to buy", filter: "All"},
+    ]
+
+    const endState = todolistReducer(startState, removeTodolistAC(todolistID1));
+
+    expect(endState.length).toBe(1);
+    expect(endState[0].id).toBe(todolistID2);
+});
+
+test('correct todolist should be added', () => {
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: TodolistType[] = [
+        {id: todolistID1, todolistTitle: "What to learn", filter: "All"},
+        {id: todolistID2, todolistTitle: "What to buy", filter: "All"},
+    ]
+
+    const endState = todolistReducer(startState, addTodolistAC(newTodolistTitle));
+
+    expect(endState.length).toBe(3);
+    expect(endState[2].todolistTitle).toBe(newTodolistTitle);
+    expect(endState[2].todolistTitle).toEqual('New Todolist');
+});
+
+test('correct todolist should change its name', () => {
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: Array<TodolistType> = [
+        {id: todolistID1, todolistTitle: "What to learn", filter: "All"},
+        {id: todolistID2, todolistTitle: "What to buy", filter: "All"}
+    ]
+    //
+    // const action = {
+    //     type: 'CHANGE-TODOLIST-TITLE',
+    //     id: todolistID2,
+    //     todolistTitle: newTodolistTitle
+    // };
+
+    const endState = todolistReducer(startState, changeTodolistTitleAC(todolistID2, newTodolistTitle));
+
+    expect(endState[0].todolistTitle).toBe("What to learn");
+    expect(endState[1].todolistTitle).toBe(newTodolistTitle);
+});
+
+test('correct filter of todolist should be changed', () => {
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let newFilter: TaskFilterType = "Completed";
+
+    const startState: TodolistType[] = [
+        {id: todolistID1, todolistTitle: "What to learn", filter: "All"},
+        {id: todolistID2, todolistTitle: "What to buy", filter: "All"}
+    ]
+
+    // const action = {
+    //     type: 'CHANGE-TODOLIST-FILTER',
+    //     id: todolistID2,
+    //     filter: newFilter
+    // };
+
+    const endState = todolistReducer(startState, changeTodolistFilterAC(todolistID2, newFilter));
+
+    expect(endState[0].filter).toBe("All");
+    expect(endState[1].filter).toBe(newFilter);
+});
