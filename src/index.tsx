0cdfbdf2ecc052c6cf2d16ad0816991e0bf43d26
yegorgursky <yahorhurski@gmail.com>
2022-09-03 22:04:52 +0300

    fixed react dom render

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,19 @@ import * as serviceWorker from './serviceWorker';
 import {AppWithReducers} from './AppWithReducers';
 import {Provider} from 'react-redux';
 import {store} from './state/store';
+import {createRoot} from 'react-dom/client';
 
 // ReactDOM.render(<App />,  document.getElementById('root'));
 // ReactDOM.render(<AppWithReducers />,  document.getElementById('root'));
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLInputElement;
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <AppWithRedux/>
     </Provider>
-    , document.getElementById('root'));
+    // , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
